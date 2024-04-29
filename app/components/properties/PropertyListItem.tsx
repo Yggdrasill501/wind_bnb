9577603cf83afcd05db81af6febd49834f3ea26f
Filip Žitný <filipzitny@gmail.com>
2024-04-29 22:28:43 +0200

    update

--- app/components/properties/PropertyListItem.tsx
+++ app/components/properties/PropertyListItem.tsx
@@ -1,6 +1,12 @@
+import Image from 'next/image';
+
 const PropertyListItem = () => {
     return (
-    <h2>Property List</h2>    
+    <div className="cursor-pointer">
+        <div className="owerflow-hidden aspect-square rounded-xl">
+            <Image src="/images/property.jpg" alt="Property" width={300} height={300}/>
+        </div>
+    </div>
     );
 }
 export default PropertyListItem;
