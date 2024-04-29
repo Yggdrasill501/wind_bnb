9c4e2a2ba7359120039da89d3e0b94700b0a9199
Filip Žitný <filipzitny@gmail.com>
2024-04-29 11:32:41 +0200

    bug fix

--- app/components/categories.tsx
+++
@@ -1,9 +0,0 @@
-const Categories = () => {
-    return (
-        <div className="max-w[1500px] mx-auto px-6">
-            <h1>Categories</h1>
-        </div>
-    );
-}
-
-export default Categories;
---
+++ app/components/categories/Categories.tsx
@@ -0,0 +1,9 @@
+const Categories = () => {
+    return(
+    <div className="max-w[1500px] mx-auto px-6">
+        <h1>Categories</h1>
+    </div>
+    );
+}
+
+export default Categories;
