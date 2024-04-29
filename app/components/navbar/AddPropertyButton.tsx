99b2372fa655fdc7cb1e990931e1f403d1e93c3e
Filip Žitný <filipzitny@gmail.com>
2024-04-29 08:35:20 +0200

    colors

--- app/components/navbar/AddPropertyButton.tsx
+++ app/components/navbar/AddPropertyButton.tsx
@@ -1,6 +1,6 @@
 const AddPropertyButton = () => {   
     return (
-        <button className="p-2 text-sm font-semibold rounded-full hover:bg-gray200">
+        <button className="p-2 text-sm font-semibold rounded-full hover:bg-wind-yellow">
             Add Widnmill
         </button>
     );
--- app/components/navbar/SearchFilters.tsx
+++ app/components/navbar/SearchFilters.tsx
@@ -3,26 +3,26 @@ const SearchFilters = () => {
         <div className="h-[64px] flex flex-row items-center justify-between border rounded-full">
             <div className="hidden lg:block">
                 <div className="flex flex-row items-center justify-between">
-                    <div className="cursor-pointer w-[250px] h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100"> 
+                    <div className="cursor-pointer w-[250px] h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-wind-yellow"> 
                         <p className="text-xm font-semibold">Where</p>
                         <p className="text-sm">Location</p>
                     </div>
-                     <div className="cursor-pointer h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100"> 
+                     <div className="cursor-pointer h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-wind-yellow"> 
                         <p className="text-xm font-semibold">Check In</p>
                         <p className="text-sm">Add dates</p>
                     </div>
-                     <div className="cursor-pointer h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100"> 
+                     <div className="cursor-pointer h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-wind-yellow"> 
                         <p className="text-xm font-semibold">Check Out</p>
                         <p className="text-sm">Add dates</p>
                     </div>
-                    <div className="cursor-pointer h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100"> 
+                    <div className="cursor-pointer h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-wind-yellow"> 
                         <p className="text-xm font-semibold">Who</p>
                         <p className="text-sm">Add guests</p>
                     </div>
                 </div>
             </div>
             <div className="p-2">
-                <div className="cursor-pointer p-2 lg:p-4 bg-airbnb hover:bg-windbnb-dark transition rounded-full text-white">
+                <div className="cursor-pointer p-2 lg:p-4 bg-airbnb hover:bg-wind-dark transition rounded-full text-white">
                     <svg
                         viewBox="0 0 32 32" 
                         style={{display:'block', fill:'none', height: '16px', width: '16px', stroke: 'currentColor', strokeWidth:4, overflow:'visible'}} 
