99d590267c9cae0c531a812e6893b8bf862c93d7
Filip Žitný <filipzitny@gmail.com>
2024-04-29 10:54:01 +0200

    update

---
+++ Categories.tsx
@@ -0,0 +1,11 @@
+" ============================================================================
+" Netrw Directory Listing                                        (netrw v171)
+"   /Users/yggdrasill501/Projects/code/wind_bnb/app/components
+"   Sorted by      name
+"   Sort sequence: [\/]$,\<core\%(\.\d\+\)\=\>,\.h$,\.c$,\.cpp$,\~\=\*$,*,\.o$,\.obj$,\.info$,\.swp$,\.bak$,\~$
+"   Quick Help: <F1>:help  -:go up dir  D:delete  R:rename  s:sort-by  x:special
+" ==============================================================================
+../
+./
+navbar/
+Categories.tsx
