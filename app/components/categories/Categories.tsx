6a19c9f03fdd0a959667f8f63bd57a6a277083e1
Filip Žitný <filipzitny@gmail.com>
2024-04-29 12:27:39 +0200

    update

--- app/components/categories/Categories.tsx
+++ app/components/categories/Categories.tsx
@@ -2,8 +2,8 @@ const Categories = () => {
     return(
     <div className="pt-3 cursor-pointer pb-6 flex items-center space-x-12">
         <div className=" pb-4 flex-col items-center space-y-2 border-b-2 borde-white hover:opacity-100">
-            <svg viewBox="0 0 24 24" style = {{display:'block', fill:'none', height:'6', width:'6', stroke:'currentColor', strokeWidth:1.5, overflow:'visible'}}> 
-                <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205 3 1m1.5.5-1.5-.5M6.75 7.364V3h-3v18m3-13.636 10.5-3.819"/>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205 3 1m1.5.5-1.5-.5M6.75 7.364V3h-3v18m3-13.636 10.5-3.819" />
             </svg>
         </div>    
     </div>
