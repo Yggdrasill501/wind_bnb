b31e3ec1458dd967b7b2f107ac83e3184b2260a0
Filip Žitný <filipzitny@gmail.com>
2024-04-29 12:34:01 +0200

    update

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Categories from "./components/categories/Categories";
 
 export default function Home() {
   return (
-    <main className="max-w-[1500px] mx-auto px-6">
+    <main className="max-w-[1500px] mx-auto px-8">
         <Categories/>
     </main>
   );
